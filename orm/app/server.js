2e9443c7207da74d44cfa32fa2f50999f68c34fb
jhta <jeisonhs93@gmail.com>
2017-11-22 20:02:56 -0500

    add nodemon

--- orm/app/server.js
+++ orm/app/server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('Hello world');
+  res.write('Hello world...');
   res.end();
 
 });
 
 server.listen(4000, () => {
-  console.log("server listen for port 4000 :D");
+  console.log("server listen for port 4000 :3");
 });
 
 
