07ccf311f28d90fc166f3f7f5f4d5b6f5b71ddf7
jhta <jeisonhs93@gmail.com>
2017-11-28 00:45:16 -0500

    connect postgres with node via docker

--- orm/app/server.js
+++ orm/app/server.js
@@ -1,6 +1,23 @@
 const http = require('http');
+const Sequelize = require('sequelize');
+const configDB = {
+  host: 'localhost',
+  dialect: 'postgres',
+  port: '5432',
+  database: 'test'
+};
 
 const server = http.createServer((req, res) => {
+  const sequelize = new Sequelize('postgresql://postgres:123456@postgres:5432/test');
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('connection has been established successfully');
+    })
+    .catch((err) => {
+      console.log('paila pana', err.message);
+    });
+
   res.write('Hello world...');
   res.end();
 
