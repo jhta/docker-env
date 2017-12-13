3d0ce67b3ceddfefdc4e74f8f718563a870d2b24
jhta <jeisonhs93@gmail.com>
2017-12-13 09:20:12 -0500

    metric model

--- orm/app/index.js
+++ orm/app/index.js
@@ -2,6 +2,7 @@
 
 const setupDatabase = require('./lib/db')
 const setupAgent = require('./lib/agent')
+const setupMetric = require('./lib/metric')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 const defaults = require('defaults')
@@ -33,7 +34,7 @@ module.exports = async function (config) {
   }
 
   const Agent = setupAgent(AgentModel)
-  const Model = {}
+  const Model = setupMetric(MetricModel)
 
   return {
     Agent,
